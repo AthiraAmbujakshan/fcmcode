b7a7233312e2283001fd28db1238ad74ed529c64
AthiraAmbujakshan <63126142+AthiraAmbujakshan@users.noreply.github.com>
2022-10-18 22:29:19 +0530

    Update Projects.java

--- fcmfinalcode/src/main/java/com/cognizant/test/module/data/Projects.java
+++ fcmfinalcode/src/main/java/com/cognizant/test/module/data/Projects.java
@@ -1,1 +1,46 @@
+package com.cognizant.test.module.data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="projectsinfo")
+public class Projects {
+
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private int id;
+	private String projectname;
+	
+	public Project() {
+		
+		
+	}
+
+	public Project(int id, String projectname) {
+		super();
+		this.id = id;
+		this.projectname = projectname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProjectname() {
+		return projectname;
+	}
+
+	public void setProjectname(String projectname) {
+		this.projectname = projectname;
+	}
+
+}
